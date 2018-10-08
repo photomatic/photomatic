49961e3e3259ab71ebd1350ddf3faed48072c994
Steak <>
2018-10-08 19:54:38 +0200

    Added auto-import

--- main_photo.py
+++ main_photo.py
@@ -25,6 +25,17 @@ from glob import glob
 import pyudev
 import psutil
 import os.path
+
+try:
+  import requests
+except ImportError:
+  print "Trying to Install required module: requests\n"
+  os.system('python -m pip install requests')
+  import requests
+# -- above lines try to install requests module if not present
+# -- if all went well, import required module again ( for global access)
+
+
 import requests
 
 
